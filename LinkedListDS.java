664767dd86f0bf40c52f51ad639630c159736a96
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-30 20:59:15 +0530

    Welcome Messsage

---
+++ LinkedListDS.java
@@ -0,0 +1,7 @@
+package com.bl.ds.linkedlist;
+
+public class LinkedListDS {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Linked List Implementation Program");
+	}
+}
